2efcd0e6f6051c40139ef7d77bf9857564c9dcb6
Tom Claudio <dev.tomclaudio@gmail.com>
2023-11-09 16:07:21 +0800

    create script to start express typescript

---
+++ create-express-typescript.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "Creating project..."
+
+if [ $# -eq 0 ];
+then
+  echo "Please provide DIR name"
+  exit 1
+else 
+  mkdir $1
+  cd $1
+  npm init -y
+  npm install express dotenv cors bcryptjs express-async-handler jsonwebtoken
+  npm install -D typescript nodemon ts-node @types/express @types/node @types/cors @types/bcryptjs @types/jsonwebtoken
+  npx tsc -init
+  mkdir src
+  mkdir src/routes
+  mkdir src/controllers
+  mkdir src/middlewares
+  mkdir src/config
+  echo "Project created success!"
+fi
